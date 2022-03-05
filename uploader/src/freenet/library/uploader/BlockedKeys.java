3240342ce5a9f0aff2da3afdaf0d639f707d33e8
Debora Wöpcke <deboradub@gmail.com>
2022-03-05 12:01:32 +0100

    Factor out common parts of the key store

--- uploader/src/freenet/library/uploader/BlockedKeys.java
+++ uploader/src/freenet/library/uploader/BlockedKeys.java
@@ -1,14 +1,6 @@
 package freenet.library.uploader;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.net.MalformedURLException;
-import java.util.HashSet;
-import java.util.Set;
 
 import freenet.library.io.FreenetURI;
 
@@ -19,13 +11,8 @@ import freenet.library.io.FreenetURI;
  * In the first implementation this is a list of lines with one
  * FreenetURI on each line.
  */
-class BlockedKeys {
+class BlockedKeys extends StoredKeys {
 
-	private static String filename = UploaderPaths.BASE_FILENAME_DATA + "blocked";
-
-	private Set<FreenetURI> list = new HashSet<FreenetURI>();
-
-	private File directory;
 	/**
 	 * Constructor.
 	 * @param directory 
@@ -33,28 +20,7 @@ class BlockedKeys {
 	 */
 	
 	public BlockedKeys(File dir, boolean doAll) {
-		directory = dir;
-		if (!doAll) {
-			// Read all
-			final File file = new File(directory, filename);
-			try {
-				FileReader fr = new FileReader(file);
-				BufferedReader br = new BufferedReader(fr);
-				String line;
-				while ((line = br.readLine()) != null) {
-					try {
-						list.add(new FreenetURI(line));
-					} catch (MalformedURLException e) {
-						// This shouldn't happen. Ignore this URI.
-					}
-				}
-			} catch (FileNotFoundException e) {
-				// There is no such file. That is fine.
-			} catch (IOException e) {
-				// We suddenly couln't read this file. Strange problem.
-				throw new RuntimeException(e);
-			}
-		}
+		super(dir, UploaderPaths.BASE_FILENAME_DATA + "blocked", !doAll);
 	}
 
 	public void block(FreenetURI page) {
@@ -62,30 +28,7 @@ class BlockedKeys {
 	}
 
 	public void flush() {
-		String newFilename = filename + ".new";
-		File newFile = new File(directory, newFilename);
-		FileWriter fw = null;
-		try {
-			fw = new FileWriter(newFile, false);
-			for (FreenetURI uri : list) {
-				fw.write(uri.toString() + "\n");
-			}
-			fw.close();
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		} finally {
-			if (fw != null) {
-				try {
-					fw.close();
-				} catch (IOException e) {
-					throw new RuntimeException(e);
-				}
-			}
-		}
-		final File file = new File(directory, filename);
-		file.delete();
-		newFile.renameTo(file);
+		super.flush();
 		System.out.println("Now " + list.size() + " URIs in the block queue.");
 	}
-
 }
---
+++ uploader/src/freenet/library/uploader/StoredKeys.java
@@ -0,0 +1,81 @@
+package freenet.library.uploader;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.HashSet;
+import java.util.Set;
+
+import freenet.library.io.FreenetURI;
+
+/**
+ * Class that is the interface to a persistent store of keys.
+ *
+ * In the first implementation this is a list of lines with one
+ * FreenetURI on each line.
+ */
+
+public class StoredKeys {
+
+	protected static String filename;
+	protected Set<FreenetURI> list = new HashSet<FreenetURI>();
+	protected File directory;
+
+	public StoredKeys(File dir, String name, boolean readFile) {
+		directory= dir;
+		filename = name;
+		if (readFile) {
+			// Read all
+			final File file = new File(directory, filename);
+			try {
+				FileReader fr = new FileReader(file);
+				BufferedReader br = new BufferedReader(fr);
+				String line;
+				while ((line = br.readLine()) != null) {
+					try {
+						list.add(new FreenetURI(line));
+					} catch (MalformedURLException e) {
+						// This shouldn't happen. Ignore this URI.
+					}
+				}
+				br.close();
+			} catch (FileNotFoundException e) {
+				// There is no such file. That is fine.
+			} catch (IOException e) {
+				// We suddenly couldn't read this file. Strange problem.
+				throw new RuntimeException(e);
+			}
+		}
+	}
+
+	public void flush() {
+		String newFilename = filename + ".new";
+		File newFile = new File(directory, newFilename);
+		FileWriter fw = null;
+		try {
+			fw = new FileWriter(newFile, false);
+			for (FreenetURI uri : list) {
+				fw.write(uri.toString() + "\n");
+			}
+			fw.close();
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		} finally {
+			if (fw != null) {
+				try {
+					fw.close();
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+			}
+		}
+		final File file = new File(directory, filename);
+		file.delete();
+		newFile.renameTo(file);
+	}
+
+}
